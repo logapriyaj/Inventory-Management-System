8fe10152b2fdc4f0194797d1fdf26ac1d5e24197
logapriyaj <logapriya22032007@gmail.com>
2026-06-11 22:01:03 +0530

    Create schema.sql

---
+++ schema.sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    quantity INTEGER,
+    price REAL
+);
+
+CREATE TABLE suppliers (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    contact TEXT
+);
